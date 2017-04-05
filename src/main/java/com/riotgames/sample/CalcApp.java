d8be5e30cfdbf1c40124abd2f54351580288f0a5
JSH <arcane1028@gmail.com>
2017-04-05 10:47:22 +0900

    JSH make branch

--- src/main/java/com/riotgames/sample/CalcApp.java
+++ src/main/java/com/riotgames/sample/CalcApp.java
@@ -1,5 +1,6 @@
 package com.riotgames.sample;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Vector;
 
@@ -7,30 +8,37 @@ import java.util.Vector;
  * Calculator application
  */
 public class CalcApp {
+	
     public double calc(String[] tokens) {
         double firstOperand;
         double secondOperand;
         try {
             firstOperand = Double.parseDouble(tokens[0]);
-            if (tokens.length > 2) {
+            if (tokens.length > 2) 
+            {
                 Vector<Double> v = new Vector<>();
                 v.add(firstOperand);
                 int i = 0;
+                
                 while (i +2 < tokens.length)
                 {
-                    try {
+                    try 
+                    {
                         secondOperand = Double.parseDouble(tokens[i + 2]);
                         final Operator operator = Operator.findOperator(tokens[i+1]);
                         v.add(operator.evaluate(v.lastElement(), secondOperand));
+                        
                     }
-                    catch (NumberFormatException e) {
-                        return -1;
-                    }
+                    catch (NumberFormatException e) {  return -1; }
+                    
                     i = i + 2;
                 }
+                
                 return v.lastElement();
-            } else {
-                return firstOperand;
+                
+            } else 
+            {   
+            	return firstOperand;
             }
         }
         catch (NumberFormatException e) {
